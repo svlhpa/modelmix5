fd2b613c3fdeec332652d32cd08d2e2f79dae3dc
ArnzPhilS <aaron.santos.mentor@gmail.com>
2025-06-20 11:20:38 +0800

    Fix Vite configuration and deploy to Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: process.cwd(),
   publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
     rollupOptions: {
-      input: {
-        main: './index.html'
-      },
+      input: resolve(__dirname, 'index.html'),
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           supabase: ['@supabase/supabase-js'],
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
     },
   },
   optimizeDeps: {
