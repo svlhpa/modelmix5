83f808b723dcb3bc68582c3b12f62ed1d63dbbfb
ArnzPhilS <aaron.santos.mentor@gmail.com>
2025-06-20 11:13:46 +0800

    Fix Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
